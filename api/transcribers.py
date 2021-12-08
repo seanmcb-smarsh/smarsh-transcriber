961824b240d55eac687f578a28087fa355842628
peterwolf-smarsh <95297562+peterwolf-smarsh@users.noreply.github.com>
2021-12-08 15:02:35 -0500

    API changes from Sean and Sonal

--- api/transcribers.py
+++ api/transcribers.py
@@ -6,30 +6,127 @@ from deepscribe_inference.load_model import load_model, load_decoder
 from deepscribe_inference.transcribe import run_inference
 from trellis import PredictiveModel
 
-def transcriber_factory(yaml_file):
-    with open(yaml_file, "r") as configs:
-        settings = yaml.load(configs, Loader=yaml.FullLoader)
-        comps = settings["transcriber"].split('.')
-        clz = __import__(comps[0])
-        for comp in comps[1:]:
-            clz = getattr(clz, comp)
-        return clz(settings)
+def transcriber_factory(config):
+    """
+    Factory to produce Transcribers.
+    Passed a config object, it returns a Transcriber ready to go.
+    The Transcriber will have been initialized and loaded with all models,
+    and be ready to convert speech into text.
+
+    :param config: Config object containing parameters common to all transcribers
+
+    The config object is Duck Typed.  It doesn't matter what kind of
+    object it is, provided it has the required fields.  They are:
+
+    transcriber: string class name of the Transcriber engine e.g. DeepscribeTranscriber
+    config: a config object specific to the Transcriber engine e.g. a Deepscribe InferenceConfig
+    """
+    comps = config.transcriber.split('.')
+    clz = __import__(comps[0])
+    for comp in comps[1:]:
+        clz = getattr(clz, comp)
+    return clz(config.config)
+
+@dataclass
+class TranscriptionToken:
+    text: str
+    start_time: int
+    end_time: int
+
+@dataclass
+class TranscriptionResult:
+    tokens: Iterable[TranscriptionToken]
 
 class DeepscribeTranscriber(PredictiveModel):
+    """
+    Internal implementation of Transcriber API to Deepscribe.  Only accessed via transcriber_factory()
+    """
+    def __init__(self,inference_config):
+        """
 
-    def __init__(self,settings):
-        self.cfg = TranscribeConfig()
-        self.inf_cfg = self.cfg.inference
+        :param inference_config: Config object containing parameters specific to the DeepscribeTranscriber
+
+        The inference_config object is Duck Typed.  It doesn't matter what kind of
+        object it is, provided it has the required fields.  To allow complete control,
+        the config is expected to conform to a Deepscribe InferenceConfig class.  Described here:
+
+        https://git.corp.digitalreasoning.com/projects/LABS/repos/deepscribe-inference/browse/deepscribe_inference/config.py
+
+        @dataclass
+        class InferenceConfig:
+            decoder: DecoderConfig = DecoderConfig()
+            text_postprocessing: TextPostProcessingConfig = TextPostProcessingConfig()
+            data: DataConfig = DataConfig()
+            model: SavedModelConfig = SavedModelConfig()
+            output: OutputConfig = OutputConfig()
+            hardware: HardwareConfig = HardwareConfig()
+            verbose: bool = True  # Verbosity of logging
+
+        @dataclass
+        class DecoderConfig:
+            lm_path: str = ''  # Path to an (optional) kenlm language model for use with beam search
+            alpha: float = 0.39  # Language model weight Default is tuned for English
+            beta: float = 0.45  # Language model word bonus (all words) Default is tuned for English
+            cutoff_top_n: int = 40  # Keep top cutoff_top_n characters with highest probs in beam search
+            cutoff_prob: float = 1.0  # Cutoff probability in pruning. 1.0 means no pruning
+            lm_workers: int = 8  # Number of LM processes to use for beam search
+            beam_width: int = 32  # Beam width to use for beam search
+            decoder: DecoderType = DecoderType.greedy  # Decoder type to use (automatically set to beam when lm_path specified)
+
+        @dataclass
+        class TextPostProcessingConfig:
+            punc_path: str = ''  # Path to a DeepScribe Punctuation model
+            acronyms_path: str = ''  # Path to acronym whitelist (collapse and capitalize)
+
+        @dataclass
+        class SavedModelConfig:
+            model_path: str = ''  # Path to acoustic model
+            train_manifest_path: str = ''  # Path to a manifest that was used to train the model
+
+        @dataclass()
+        class HardwareConfig:
+            cuda: bool = True  # Use CUDA for inference
+            precision: ModelPrecision = ModelPrecision.half  # Precision for CUDA inference
+
+        The InterfaceConfig has many values, however default values are provided for all of them.
+        The caller overrides some of these values using recommendations from Research.
+        Some common overrides are:
+
+        model.model_path: string path to acoustic models e.g. "/share/models/english/deepscribe-0.3.0.pth"
+        decoder.lm_path: string path to language models e.g. "/share/models/english/financial-0.1.3.trie"
+        decoder.alpha: float weight for alpha e.g. 0.39
+        decoder.beta: float weight for beta e.g. 0.45
+        text_postprocessing.punc_path: string path to punctuation models e.g. "/share/models/english/punc-0.2.0.pth"
+        text_postprocessing.acronyms_path: string path to acronyms models e.g. "/share/models/english/acronyms/all.acronyms.txt"
+        hardware.cuda: boolean specify whether the engine should run on a GPU e.g. true
+        """
+        self.inf_cfg = inference_config
         self.device = torch.device("cuda" if self.inf_cfg.hardware.cuda else "cpu")
         self.model = load_model(
-            model_path=settings['model_path'],
+            model_path=self.inf_cfg.model.model_path,
             precision=self.inf_cfg.hardware.precision,
             device=self.device)
         self.decoder = load_decoder(
             decoder_cfg=self.inf_cfg.decoder,
             labels=self.model.labels)
 
-    def predict(self,input_path):
+    def predict(self,input_paths: Iterable[str]) -> Mapping[str, TranscriptionResult]:
+        """
+        Convert audio recordings into transcriptions.
+
+        :param input_paths: An iterable collection of string file paths.  Paths are expected to be WAV files
+        :return: a Mapping of file path to TranscriptionResult
+
+        @dataclass
+        class TranscriptionToken:
+            text: str
+            start_time: int
+            end_time: int
+
+        @dataclass
+        class TranscriptionResult:
+            tokens: Iterable[TranscriptionToken]
+        """
         return run_inference(
             input_path=input_path,
             model=self.model,
--- tests/test_transcribers.py
+++ tests/test_transcribers.py
@@ -1,26 +1,29 @@
 from unittest import TestCase
 
 import api.transcribers
-import api.language_identifiers
-import api.voice_activity_detectors
+#import api.language_identifiers
+#import api.voice_activity_detectors
 class Test(TestCase):
 
     def test_transcribe(self):
-        transcriber = transcriber_factory('example_configs/english.yaml')
-        result = transcriber.predict('data/english.wav')
-        print(result["data/english.wav"]["transcription"])
+        cfg = read_config('example_configs/english.yaml')
+        transcriber = transcriber_factory(cfg)
+        input = "data/english.wav"
+        result = transcriber.predict([input])
+        for token in result[input].tokens:
+            print(token.text,token.start_time,token.end_time)
 
-    def test_lid(self):
-        lid = lid_factory('example_configs/lid.yaml')
-        result = lid.predict('data/english.wav')
-        print(result["data/english.wav"]["label"])
-
-    def test_lid_intervals(self):
-        lid = lid_factory('example_configs/lid_intervals.yaml')
-        result = lid.predict({'data/english.wav':[[0, 5], [6, 10]])
-        print(result["data/english.wav"]["label"])
-
-    def test_vad(self):
-        vad = vad_factory('example_configs/vad.yaml')
-        result = vad.predict('data/english.wav')
-        print(result["data/english.wav"]["intervals"])
+    # def test_lid(self):
+    #     lid = lid_factory('example_configs/lid.yaml')
+    #     result = lid.predict('data/english.wav')
+    #     print(result["data/english.wav"]["label"])
+    #
+    # def test_lid_intervals(self):
+    #     lid = lid_factory('example_configs/lid_intervals.yaml')
+    #     result = lid.predict({'data/english.wav':[[0, 5], [6, 10]])
+    #     print(result["data/english.wav"]["label"])
+    #
+    # def test_vad(self):
+    #     vad = vad_factory('example_configs/vad.yaml')
+    #     result = vad.predict('data/english.wav')
+    #     print(result["data/english.wav"]["intervals"])
