c71d1de8f3033519818398b586a5f8bc12017edf
Thomas Heap <thomas.heap@smarsh.com>
2021-12-14 21:33:53 +0000

    Check to install requirements/dev.txt in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,13 @@ PIP="pip3"
 
 set -x
 
+if [ -z "$GITHUB_ACTIONS" ]; then
+    REQUIREMENTS="requirements/dev.txt"
+else
+    PIP="pip3"
+fi
 
 conda install -c conda-forge ffmpeg -y
-conda uninstall llvmlite -y # required due to librosa installing llvmlite in 8.0>=
 conda install -y pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=10.2 -c pytorch
 
 "$PIP" install -r "$REQUIREMENTS"
